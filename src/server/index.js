fa0c8e542299d7a0a4fb05ec8df337203e4d018b
michaelmwhite <michael.m.white@live.com>
2019-02-01 15:19:27 -0500

    added video search query to server

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const express = require('express');
 const azure = require('ms-rest-azure');
 const NewsSearchApiClient = require('azure-cognitiveservices-newssearch');
@@ -11,8 +12,40 @@ app.get('/api/search', (req, res) => {
   searchClient.newsOperations.search('joe rogan').then((result) => {
     // console.log(result.value);
     res.send({ searchData: result.value });
+    bingVideoSearch('kittens');
   }).catch((err) => {
     throw err;
   });
 });
 app.listen(8080, () => console.log('Listening on port 8080!'));
+
+function responseHandler(response) {
+  let body = '';
+  response.on('data', (d) => {
+    body += d;
+  });
+  response.on('end', () => {
+    body = JSON.stringify(JSON.parse(body), null, '  ');
+  });
+  response.on('error', (e) => {
+    throw e;
+  });
+}
+
+function bingVideoSearch(search) {
+  let subscriptionKey = '3fd7d3b07f0d4193a3ee63287ce7669e';
+  let host = 'api.cognitive.microsoft.com';
+  let path = '/bing/v7.0/videos/search';
+
+  let requestParams = {
+    method: 'GET',
+    hostname: host,
+    path: path + '?q=' + encodeURIComponent(search),
+    headers: {
+      'Ocp-Apim-Subscription-Key': subscriptionKey,
+    }
+  };
+
+  let req = https.request(requestParams, responseHandler);
+  req.end();
+}
