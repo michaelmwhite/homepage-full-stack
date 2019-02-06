a17cff0716339a82aa6402d6c58b3865905b7f26
michaelmwhite <michael.m.white@live.com>
2019-02-06 15:01:06 -0500

    added basic styling for site

--- src/client/components/NewsComponent.tsx
+++ src/client/components/NewsComponent.tsx
@@ -1,13 +1,15 @@
 import { NewsObject } from "../interfaces/NewsObject";
 import * as React from 'react';
+import * as url from 'url';
 
 export class NewsComponent extends React.Component<NewsObject, any> {
     render() {
         return (
             <div className="news-component">
-                <h2>{this.props.name}</h2>
-                <p>{this.props.description}</p>
-                <p><a href={this.props.url}>{this.props.url}</a></p>
+                <h2><a href={this.props.url}>{this.props.name}</a></h2>
+                <a href={this.props.url} className="styled-link">
+                    {url.parse(this.props.url).hostname}
+                </a>
             </div>
         );
     }
--- src/client/components/VideoComponent.tsx
+++ src/client/components/VideoComponent.tsx
@@ -1,15 +1,24 @@
 import { VideoObject } from "../interfaces/VideoObject";
 import * as React from 'react';
+import * as url from 'url';
 
 export class VideoComponent extends React.Component<VideoObject, any> {
     render() {
         return (
             <div className="video-component">
-                <h2>{this.props.name}</h2>
-                <img src={this.props.thumbnailUrl}
-                    width="180" />
-                <p>{this.props.description}</p>
-                <p><a href={this.props.contentUrl}>{this.props.contentUrl}</a></p>
+                <div className="thumbnail-container">
+                    <a href={this.props.contentUrl}>
+                        <img src={this.props.thumbnailUrl} />
+                    </a>
+                </div>
+                <div className="video-info">
+                    <a href={this.props.contentUrl}>
+                        <h2>{this.props.name}</h2>
+                    </a>
+                    <a href={this.props.contentUrl} className="styled-link">
+                        {url.parse(this.props.contentUrl).hostname}
+                    </a>
+                </div>
             </div>
         );
     }
