c99a70fa39607aace4cadad01a80359d9f0f1de2
michaelmwhite <michael.m.white@live.com>
2019-02-08 16:42:41 -0500

    added text input to header - still need to add functionality and finish styling

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -6,6 +6,13 @@ export class Header extends React.Component<any, any>{
             <nav>
                 <ul>
                     <li><a href="#top">Newspaper 21</a></li>
+                    <li>
+                        <form>
+                            <input type="text" placeholder="Add a news topic"
+                                autoCorrect="off" autoComplete="off"
+                                autoCapitalize="off" spellCheck={false} />
+                        </form>
+                    </li>
                 </ul>
             </nav>
         );
