c40c23b6a72d0f986e9a8a5754d07a3163845ab2
michaelmwhite <michael.m.white@live.com>
2019-02-24 11:32:17 -0500

    upped freshness - made results better

--- src/server/search-api.js
+++ src/server/search-api.js
@@ -1,6 +1,9 @@
 const keys = require('../keys');
 const cacheUtil = require('./cache-util');
 
+// TODO: make returned results better - booting freshness up to a week helped, but will stories get
+// changed out enough? maybe request a greater number of stories and then ignore ones in history
+
 // Good resources: https://www.tomas-dvorak.cz/posts/nodejs-request-without-dependencies/
 // https://stackoverflow.com/questions/38533580/nodejs-how-to-promisify-http-request-reject-got-called-two-times
 // MS docs: https://docs.microsoft.com/en-us/rest/api/cognitiveservices/bing-web-api-v7-reference
@@ -8,7 +11,7 @@ function bingVideoSearch(resolve, reject, search) {
     let subscriptionKey = keys.azureKey;
     let host = 'api.cognitive.microsoft.com';
     let path = '/bing/v7.0/videos/search';
-    let queryParams = 'count=3&freshness=day'
+    let queryParams = 'count=3&freshness=week'
     let requestParams = {
         method: 'GET',
         hostname: host,
@@ -24,7 +27,7 @@ function bingNewsSearch(resolve, reject, search) {
     let subscriptionKey = keys.azureKey;
     let host = 'api.cognitive.microsoft.com';
     let path = '/bing/v7.0/news/search';
-    let queryParams = 'count=3&freshness=day'
+    let queryParams = 'count=3&freshness=week'
     let requestParams = {
         method: 'GET',
         hostname: host,
