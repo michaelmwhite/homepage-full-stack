4345f21611f4d85c99db3b37c99a4ccea6d9f69a
michaelmwhite <michael.m.white@live.com>
2019-03-11 22:50:01 -0400

    show hide search dropdown properly - must still populate w/ info

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -3,6 +3,7 @@ import { appendTopicCookie } from '../utils/cookie-util';
 
 interface State {
     inputValue: string;
+    showDropdown: boolean;
 }
 interface Props {
     showOverlay: () => void;
@@ -12,20 +13,32 @@ interface Props {
 export class Header extends React.Component<Props, State>{
     constructor(props: Props) {
         super(props);
-        this.state = { inputValue: '' };
+        this.state = { inputValue: '', showDropdown: false };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.searchFocused = this.searchFocused.bind(this);
+        this.searchBlurred = this.searchBlurred.bind(this);
     }
 
     handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-        this.setState({ inputValue: event.target.value });
+        this.setState({ ...this.state, inputValue: event.target.value });
     }
 
     handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         appendTopicCookie(this.state.inputValue);
-        this.setState({ inputValue: '' });
+        this.setState({ ...this.state, inputValue: '' });
         event.preventDefault();
+    }
+
+    searchFocused() {
+        this.props.showOverlay();
+        this.setState({ ...this.state, showDropdown: true });
+    }
+
+    searchBlurred() {
+        this.props.hideOverlay();
+        this.setState({ ...this.state, showDropdown: false });
     }
 
     // Note: keeping ul-li for now since that's how apple has it, but they handle reduced screen
@@ -42,8 +55,11 @@ export class Header extends React.Component<Props, State>{
                             autoCorrect="off" autoComplete="off"
                             autoCapitalize="off" spellCheck={false}
                             value={this.state.inputValue} onChange={this.handleChange}
-                            onFocus={this.props.showOverlay} onBlur={this.props.hideOverlay} />
+                            onFocus={this.searchFocused} onBlur={this.searchBlurred} />
                     </form>
+                    <section className={this.state.showDropdown ? "search-dropdown" : "hidden-dropdown"}>
+                        TODO: Results...
+                    </section>
                 </aside>
             </nav>
         );
