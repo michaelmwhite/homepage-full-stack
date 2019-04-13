1bc2b4b09949a1ccf92e2af400d8a4a2aa147f37
michaelmwhite <michael.m.white@live.com>
2019-04-13 12:47:37 -0400

    Revert "configured things for github pages"
    This reverts commit bfb2fa49fda7aebdf90c35c48cb3d56b8c02ad84.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const outputDirectory = 'build';
+const outputDirectory = 'dist';
 
 module.exports = {
   entry: './src/client/index.tsx',
