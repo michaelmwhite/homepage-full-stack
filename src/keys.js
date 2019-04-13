20c67bdd7ecff90c406a4e4c0229f523512e1c17
michaelmwhite <michael.m.white@live.com>
2019-04-13 12:47:24 -0400

    Revert "Add necessary hosting files"
    This reverts commit a8fd226a83fc69fca7661aacca3deec5a667d214.

--- src/keys.js
+++
@@ -1,6 +0,0 @@
-// in order to host using github pages, must have the key publickly available here, but I am using the free pricing tier on Azure
-const azureKey = '3fc0e37b9bf84c61b03d8d9acb9c70fb';
-
-module.exports = {
-    azureKey: azureKey
-}
