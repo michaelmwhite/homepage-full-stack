fe8da059d3f8d453c439e176ec0a3df9fb706d28
michaelmwhite <michael.m.white@live.com>
2019-04-15 18:30:43 -0400

    updated script src dependencies

--- src/server/index.js
+++ src/server/index.js
@@ -22,4 +22,5 @@ app.get('/api/video/search/:topic', (req, res) => {
       throw err;
     });
 });
-app.listen(8080, () => console.log('Listening on port 8080!'));
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('Listening on port ' + port + '!'));
